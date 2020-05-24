780e6c0b2b5ba8b0ff4839d5a833d07426ba1252
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-05-24 07:12:56 -0700

    Initial Commit

---
+++ CleffyNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleffyNotes
+//
+//  Created by Cora Jacobson on 5/24/20.
+//  Copyright © 2020 Cora Jacobson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
